c9d3943ca0b30787895dc185a8ce2141b350f919
Fabio Z Tessitore <fabioztessitore@gmail.com>
2026-01-05 13:21:50 +0100

    rendering condizionale e stili dinamici

--- 16_react_eventi/app/components/Counter.tsx
+++ 16_react_eventi/app/components/Counter.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Button from "./Button";
+import Flag from "./Flag";
 
 export default function Counter() {
   const [counter, setCounter] = useState(0);
@@ -14,13 +15,28 @@ export default function Counter() {
     setCounter(counter - 1);
   }
 
+  function showFlag() {
+    return counter >= 10;
+  }
+
+  function warning() {
+    return counter < 0;
+  }
+
   return (
-    <div className="p-4 text-xl bg-gray-800 flex rounded-lg">
-      <Button action={decrement} text="-" />
-      <span className="text-xl flex justify-center w-50 items-center text-gray-200">
-        {counter}
-      </span>
-      <Button action={increment} text="+" />
+    <div className="flex items-center">
+      <div
+        className={`p-4 text-xl flex rounded-lg items-center ${
+          warning() ? "bg-red-500" : "bg-gray-800"
+        }`}
+      >
+        <Button action={decrement} text="-" />
+        <span className="text-xl flex justify-center w-50 items-center text-gray-200">
+          {counter}
+        </span>
+        <Button action={increment} text="+" />
+      </div>
+      {showFlag() && <Flag />}
     </div>
   );
 }
